471ae4f77bac3766007052c5d35a96dcc3ebf2fa
Evan <evanmgreen063@gmail.com>
2020-09-21 12:52:26 -0400

    testbench added o1=0

--- AND_Gate/testbench.sv
+++ AND_Gate/testbench.sv
@@ -1,1 +1,19 @@
-//code here
+`timescale 1ns/1ps
+
+module testbench();
+  reg a1,b1; //reg holds values at this location
+  wire o1; //has no memory
+  
+  AND T(a1, b1, o1);
+  
+  initial begin
+    
+//set two inputs to 1 and 0
+    a1 = 1'b1;
+    b1 = 1'b0;
+    
+    $display("o1=%b", o1);
+    
+  end
+  
+endmodule
