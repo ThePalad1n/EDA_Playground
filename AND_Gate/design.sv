e4a3b63bbbd9a2c8d5f6a9f2fe1914fdc571d37d
Evan <evanmgreen063@gmail.com>
2020-09-21 12:51:06 -0400

    design code

---
+++ AND_Gate/design.sv
@@ -0,0 +1,7 @@
+module AND(A,B,O);
+  input A;
+  input B;
+  output O;
+  
+  and G(O,A,B); //built in AND function
+endmodule
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # EDA_Playground
+##Testing different gates and circuits on a virtual Icarus Verilog 0.9.7
